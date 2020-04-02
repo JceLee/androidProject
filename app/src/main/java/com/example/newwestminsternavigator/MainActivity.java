814869c1279a47bbe62208da1e15db4d4d747f73
Kian Darakhshan <kian.d@outlook.com>
2020-04-01 20:35:51 -0700

    Added button functionality to open new activities

--- app/src/main/java/com/example/newwestminsternavigator/MainActivity.java
+++ app/src/main/java/com/example/newwestminsternavigator/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.newwestminsternavigator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startMap(View v) {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void openSettings(View v) {
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void exitApp(View v) {
+        finish();
+        System.exit(0);
+    }
+
+
 }
